ba9b787778858bcf3a566bf9f4f448a1de68dc43
Antonio Florio <AFlorio45@gmail.com>
2023-03-06 12:30:02 +0100

    Initial commit

---
+++ DestinationWeather.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DestinationWeather.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DestinationWeather.MVC
+ 
