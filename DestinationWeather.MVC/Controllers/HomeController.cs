739c1ee683467cdc20c8e62640dc2eea448546b7
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-03-06 22:59:55 +0100

    update

--- DestinationWeather.MVC/Controllers/HomeController.cs
+++ DestinationWeather.MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using GoogleMaps.LocationServices;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace DestinationWeather.MVC.Controllers
 {
@@ -25,10 +26,10 @@ namespace DestinationWeather.MVC.Controllers
             return View();
         }
 
-        public Data Search([Bind("start, destination")] Data data)
+        public async Task<Object> SearchAsync([Bind("start, destination")] Data data)
         {
             //dotnet dev-certs https --trust
-            var locationService = new GoogleLocationService();
+            /*var locationService = new GoogleLocationService();
 
             var startAddress = data.start;
             var destinationAddress = data.destination;
@@ -45,7 +46,33 @@ namespace DestinationWeather.MVC.Controllers
             return new Data() { startLat = startLatitude.ToString(),
                                 startLon = startLongitude.ToString(),
                                 destinationLat = destinationLatitude.ToString(),
-                                destinationLon = destinationlongitude.ToString()};
+                                destinationLon = destinationlongitude.ToString()};*/
+            try
+            {
+                var apiUrl = @"http://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=Fano&format=json&limit=1";
+                using (var client = new HttpClient())
+                {
+
+                    var httpClient = new HttpClient();
+
+                    var request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+
+                    var productValue = new ProductInfoHeaderValue("ScraperBot", "1.0");
+                    var commentValue = new ProductInfoHeaderValue("(+http://www.API.com/ScraperBot.html)");
+
+                    request.Headers.UserAgent.Add(productValue);
+                    request.Headers.UserAgent.Add(commentValue);
+
+                    var resp = await httpClient.SendAsync(request);
+
+                    var datas = await resp.Content.ReadAsStringAsync();
+                    return datas;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
