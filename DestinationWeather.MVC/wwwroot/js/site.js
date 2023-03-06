5121e9a414350a36de33fcb66fc169140080aaf9
Antonio Florio <AFlorio45@gmail.com>
2023-03-06 15:29:50 +0100

    update

--- DestinationWeather.MVC/wwwroot/js/site.js
+++ DestinationWeather.MVC/wwwroot/js/site.js
@@ -1,27 +1,20 @@
-﻿var markers[];
+﻿
+var marker1;
+var marker2;
 var map = L.map('map').setView([41.29, 12.27], 5);
+
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-/*var marker = L.marker([41.29, 12.27]).addTo(map);*/
-
-/*marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();*/
-
-//var popup = L.popup()
-//    .setLatLng([51.513, -0.09])
-//    .setContent("I am a standalone popup.")
-//    .openOn(map);
-
-
-
 function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
+    /*alert("You clicked the map at " + e.latlng);*/
     var marker = L.marker(e.latlng).addTo(map)
     marker.bindPopup("<b>" + e.location + "</b><br>I am a popup.").openPopup();
-    markers += marker;
+    markers.push(marker);
 }
 
 map.on('click', onMapClick);
@@ -29,13 +22,23 @@ map.on('click', onMapClick);
 function search() {
     var start = document.forms["modulo"]["start"].value;
     var destination = document.forms["modulo"]["destination"].value;
-    $.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q=' + start, function (data) {
-        console.log(data);
-        var marker = L.marker(data.latlng).addTo(map)
-        marker.bindPopup("<b>"+start+"</b><br>").openPopup();
-    });
-    $.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q=' + destination, function (data) {
-        var marker = L.marker(data.latlng).addTo(map)
-        marker.bindPopup("<b>" + destination + "</b><br>").openPopup();
-    });
+    address1 = jQuery(start).val();
+    address2 = jQuery(destination).val();
+
 }
+
+// 1. Initialize GeoCoder
+const geocoder = new google.maps.Geocoder();
+
+// 2. The text address that you want to convert to coordinates
+let address = "Plaza de Bolívar de Bogotá";
+
+// 3. Obtain coordinates from the API
+geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+        // Display response in the console
+        console.log(results);
+    } else {
+        alert("Geocode error: " + status);
+    }
+});
