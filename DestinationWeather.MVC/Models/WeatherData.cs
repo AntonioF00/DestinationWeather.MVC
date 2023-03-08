7d9f4571217ff4cdcfafe69b6076e9672db11384
Antonio Florio <AFlorio45@gmail.com>
2023-03-08 09:00:51 +0100

    update

--- DestinationWeather.MVC/Models/WeatherData.cs
+++ DestinationWeather.MVC/Models/WeatherData.cs
@@ -26,6 +26,8 @@ namespace DestinationWeather.MVC.Models
             public string message { get; set; }
             public int cnt { get; set; }
             public List<List> list { get; set; }
+            public Coord coord { get; set; }
+            public Metrics metrics { get; set; }
             public City city { get; set; }
         }
 
