fa481355731b64d39783d339e8937d287fc81435
Antonio Florio <AFlorio45@gmail.com>
2023-03-10 10:24:04 +0100

    update

--- DestinationWeather.MVC/Models/Data.cs
+++ DestinationWeather.MVC/Models/Data.cs
@@ -19,7 +19,7 @@
         public double lat { get; set; }
         public double lon { get; set; }
         public string display_name { get; set; }
-        public string latlng { get; set; }
+        public string? latlng { get; set; }
     }
 }
 
